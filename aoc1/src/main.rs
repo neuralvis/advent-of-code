ff3230986109f7c082adb10afdb898fc8382a457
Madhu Srinivasan <madhu.srinivasan@outlook.com>
2021-12-20 20:54:06 -0800

    completed advent of code day 1

--- aoc1/src/main.rs
+++ aoc1/src/main.rs
@@ -1,15 +1,36 @@
 use std::fs::File;
-use std::io::{BufReader,BufRead, Read};
-
+use std::io::{BufRead, BufReader, Read};
 
 fn main() {
     let f: File = File::open("/Users/srinivm/develop/aoc/aoc1/input").unwrap();
     let reader: BufReader<File> = BufReader::new(f);
 
-    let array:Vec<i32> = reader.lines().map(|item| item.unwrap().to_string().parse::<i32>().unwrap()).collect::<Vec<i32>>();
-    let num:i32 = array.iter().zip(array.iter().skip(1)).fold(0,
-    |accum:i32, (&item1, &item2)| {
-        accum + (item2 > item1) as i32
-    });
+    let array: Vec<i32> = reader
+        .lines()
+        .map(|item| item.unwrap().to_string().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    part_1(&array);
+    part_2(&array);
+}
+
+fn part_1(array: &Vec<i32>) {
+    let num: i32 = array
+        .iter()
+        .zip(array.iter().skip(1))
+        .fold(0, |accum: i32, (&item1, &item2)| {
+            accum + (item2 > item1) as i32
+        });
+    println!("{}", num);
+}
+
+fn part_2(array: &Vec<i32>) {
+    let num: i32 =
+        array
+            .windows(3)
+            .zip(array.windows(3).skip(1))
+            .fold(0, |accum: i32, (item1, item2)| {
+                accum + ((item2[0] + item2[1] + item2[2]) > (item1[0] + item1[1] + item1[2])) as i32
+            });
     println!("{}", num);
 }
